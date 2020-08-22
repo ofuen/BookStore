747cda18eb0961c8a61d2d4e156a2df3d4a88cf3
oscar <oscar@o2webdev.com>
2020-08-21 20:24:17 -0400

    Modify the Details action method to return view

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -13,12 +13,37 @@ namespace BookStore.Controllers
         {
             return View();
         }
-            
 
-
-        public string Details()
+        public IActionResult Details()
         {
-            return "This is the details of a book.";
+            Book book = new Book()
+            {
+                Id = 1,
+                Title = "Learning ASP.NET Core 2.0",
+                Genre = "Programming & Software Development",
+                Price = 45,
+                PublishDate = new System.DateTime(2012, 04, 23),
+                Authors = new List<string> { "Jason De Oliveira", "Michel Bruchet" }
+            };
+
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to add the book to DB
+                return RedirectToAction("Index");
+            }
+            return View(book);
         }
     }
 }
