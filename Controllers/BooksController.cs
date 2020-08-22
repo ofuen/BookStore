841bd42a3be5afbbf56cb5381bd3bc955752a285
oscar <oscar@o2webdev.com>
2020-08-21 20:15:30 -0400

    :zap: Add a new class Book into the Models

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
@@ -12,6 +13,8 @@ namespace BookStore.Controllers
         {
             return View();
         }
+            
+
 
         public string Details()
         {
